9be0285b3f7affd480ff8099ec6d1a28e886371c
Declan Atkins <declanatkins@gmail.com>
2019-05-06 21:19:38 +0100

    Added loading of document words to vector

---
+++ include/document.h
@@ -0,0 +1,27 @@
+#ifndef DOCUMENT_H
+#define DOCUMENT_H
+
+#include <exception>
+#include <fstream>
+#include <map>
+#include <vector>
+#include "python_include.h"
+
+struct document_exception : std::runtime_error{
+    document_exception(const std::string& message) : std::runtime_error(message){}
+};
+
+class Document{
+private:
+    std::vector<std::string> words;
+    std::string filename;
+    void load_words();
+    void remove_stopwords();
+    void compute_term_frequency();
+public:
+    std::map<std::string, int> tf;
+    std::map<std::string, double> tf_idf;
+    Document(std::string document_filename);
+    std::map<std::string, double> compute_tf_idf(std::map<std::string, int>);
+};
+#endif
--- source/document.cpp
+++ source/document.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
-
+#include <fstream>
+#include <map>
+#include <vector>
 #include "document.h"
 #include "python_include.h"
 
+Document::Document(std::string f_name){
+    filename = f_name;
+    load_words();
+    //remove_stopwords();
+    //compute_term_frequency();
+}
+
+void Document::load_words(){
+    std::ifstream the_document;
+    the_document.open(filename);
+    if (the_document.is_open()){
+        for(std::string word; the_document >> word;){
+            words.push_back(word);
+        }
+    }
+    else{
+        throw document_exception("Could not load the file");
+    }
+}
+
+
+int main(){
+    Document d("test_data/test_document.txt");
+}
