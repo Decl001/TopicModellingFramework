37efa7de2bf94a983425e4e8072d433817a48a8c
Declan Atkins <declanatkins@gmail.com>
2019-05-07 22:38:07 +0100

    Added removal of stopwords

--- include/document.h
+++ include/document.h
@@ -6,22 +6,24 @@
 #include <map>
 #include <vector>
 
+#define STOP_WORDS_FILE "data/english_stopwords.txt"
+
 struct document_exception : std::runtime_error{
     document_exception(const std::string& message) : std::runtime_error(message){}
 };
 
 class Document{
 private:
-    std::vector<std::string> words;
+    std::vector<std::wstring> words;
     std::string filename;
     void load_words();
     void stem_words();
     void remove_stopwords();
     void compute_term_frequency();
 public:
-    std::map<std::string, int> tf;
-    std::map<std::string, double> tf_idf;
+    std::map<std::wstring, int> tf;
+    std::map<std::wstring, double> tf_idf;
     Document(std::string document_filename);
-    std::map<std::string, double> compute_tf_idf(std::map<std::string, int>);
+    std::map<std::wstring, double> compute_tf_idf(std::map<std::wstring, int>);
 };
 #endif
--- source/document.cpp
+++ source/document.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <map>
@@ -16,7 +17,7 @@ void Document::load_words(){
     std::ifstream the_document;
     the_document.open(filename);
     if (the_document.is_open()){
-        for(std::string word; the_document >> word;){
+        for(std::wstring word; the_document >> word;){
             words.push_back(word);
         }
     }
@@ -25,7 +26,30 @@ void Document::load_words(){
     }
 }
 
+void Document::stem_words(){
+    
+}
+
 void Document::remove_stopwords(){
+    std::vector<std::wstring> stop_words_removed;
+    std::vector<std::wstring> stop_words;
+    std::ifstream stop_words_file;
+    stop_words_file.open(STOP_WORDS_FILE);
+    if (stop_words_file.is_open()){
+        for(std::wstring word; stop_words_file >> word;){
+            stop_words.push_back(word);
+        }
+    }
+    else{
+        throw document_exception("Could not load the stop words file");
+    }
+
+    for(auto it = words.begin(); it != words.end(); it++){
+        if (std::find(stop_words.begin(), stop_words.end(), *it) == stop_words.end()){
+            stop_words_removed.push_back(*it);
+        }
+    }
+    words = stop_words_removed;
 }
 
 int main(){
